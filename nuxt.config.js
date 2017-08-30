132f6265f0590aeeb360768122744a25c9780c20
Takuma Hanatani <mail@potato4d.me>
2017-08-30 17:46:13 +0900

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,10 +7,10 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: "HANATANI Takuma a.k.a. Potato4d" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' }
     ]
   },
   /*
